a9bb4b8050bb18cb8a5f1b06db53fc355ba9703b
Adrien Vergnaud <a.vergnaud@gmail.com>
2019-01-22 22:50:59 +0100

    suite

--- listener.js
+++ listener.js
@@ -22,7 +22,10 @@ sock.on('message', msg => {
         console.log(`AMOUNT`, data[3])
         console.log(`BUNDLE`, data[8])
         console.log(`TAG`, data[12])
-        const pythonProcess = spawn('python3',["/home/ubuntu/dev4/smart-city/tetris/workspace/src/tetris.py", "adrien"]);
+        const pythonProcess = spawn('python3',["/home/ubuntu/dev4/smart-city/tetris/workspace/src/tetris_snes.py", "adrien"]);
+        pythonProcess.stdout.on('data', (data_python) => {
+          console.log(data_python)
+        });
       }
       break
     case 'sn':
